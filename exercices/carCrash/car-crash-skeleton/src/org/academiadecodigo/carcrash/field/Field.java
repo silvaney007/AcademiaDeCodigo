7307eb36fbb7a149e05b20c6d212313346db4f6a
dsrules007 <sousilvaney@gmail.com>
2020-06-15 00:21:28 +0100

    output color 2.0

--- exercices/carCrash/car-crash-skeleton/src/org/academiadecodigo/carcrash/field/Field.java
+++ exercices/carCrash/car-crash-skeleton/src/org/academiadecodigo/carcrash/field/Field.java
@@ -64,7 +64,7 @@ public final class Field {
                 }else if( c instanceof Mustang){
                     screen.putString(c.getPos().getCol(), c.getPos().getRow(), c.toString(), Terminal.Color.WHITE, Terminal.Color.CYAN);
                 }else if(c instanceof Ambulance){
-                    screen.putString(c.getPos().getCol(), c.getPos().getRow(), c.toString(), Terminal.Color.GREEN, Terminal.Color.WHITE, ScreenCharacterStyle.Blinking);
+                    screen.putString(c.getPos().getCol(), c.getPos().getRow(), c.toString(), Terminal.Color.RED, Terminal.Color.WHITE, ScreenCharacterStyle.Blinking);
                 }else if(c instanceof Tank){
                     screen.putString(c.getPos().getCol(), c.getPos().getRow(), c.toString(), Terminal.Color.WHITE, Terminal.Color.GREEN);
                 }else {
